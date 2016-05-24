26e5cf2c76668a504f3cbd5b91501793c9d169bf
Gergely Brautigam <skarlso777@gmail.com>
2016-05-24 20:20:36 +0200

    Added comparison to built in function.

--- phpalgo/1.1/exercise1.php
+++ phpalgo/1.1/exercise1.php
@@ -1,5 +1,12 @@
 <?php
 
+function DisplayElapsedTime($startTime) {
+    $time_end = microtime(true);
+    $execution_time = $time_end - $startTime;
+
+    printf("Total Execution Time: %f\n", $execution_time);
+}
+
 function toBinary($n) {
     $s = "";
     for ($i=0; 2 ** $i < $n; $i++) {
@@ -8,4 +15,11 @@ function toBinary($n) {
     return strrev($s);
 }
 
+
+$time_start = microtime(true);
 printf("%s\n", toBinary(170));
+DisplayElapsedTime($time_start);
+
+$time_start = microtime(true);
+printf("%s\n", decbin(170));
+DisplayElapsedTime($time_start);
